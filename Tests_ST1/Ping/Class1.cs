f2c339325bdb020b334fcb5db574519cb7ad6697
compaycomp <159130233+compaycomp@users.noreply.github.com>
2024-04-21 17:45:51 +0300

    Add files via upload

---
+++ Tests_ST1/Ping/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests_ST1.Ping
+{
+    public static class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+}
